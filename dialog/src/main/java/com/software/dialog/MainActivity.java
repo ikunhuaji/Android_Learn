516748939e3e7bdcd50abe4a68bb3f9df4086ce5
ikunhuaji <1550309783@qq.com>
2024-05-11 07:57:30 +0800

    finish dialog

--- dialog/src/main/java/com/software/dialog/MainActivity.java
+++ dialog/src/main/java/com/software/dialog/MainActivity.java
@@ -3,11 +3,16 @@ package com.software.dialog;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.Timer;
@@ -21,6 +26,8 @@ public class MainActivity extends AppCompatActivity{
     private Button btn_4;
     private Button btn_5;
     private Button btn_6;
+    private Button btn_7;
+    private Button btn_8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,6 +90,7 @@ public class MainActivity extends AppCompatActivity{
         });
 
         btn_3.setOnClickListener(v->{
+            //单选对话框
             final String items[] = {"我是选项一", "我是选项二", "我是选项三", "我是选项四"};
             AlertDialog dialog = new AlertDialog.Builder(this)
                     .setIcon(R.mipmap.icon)//设置标题的图片
@@ -110,6 +118,7 @@ public class MainActivity extends AppCompatActivity{
         });
 
         btn_4.setOnClickListener(v->{
+            //多选对话框
             final String items[] = {"我是选项一", "我是选项二", "我是选项三", "我是选项四"};
             final boolean checkedItems[] = {true, false, true, false};
             AlertDialog dialog = new AlertDialog.Builder(this)
@@ -144,12 +153,14 @@ public class MainActivity extends AppCompatActivity{
         });
 
         btn_5.setOnClickListener(v->{
+            //圆形加载框
             ProgressDialog dialog = new ProgressDialog(this);
             dialog.setMessage("正在加载中");
             dialog.show();
         });
 
         btn_6.setOnClickListener(v->{
+            //条形加载框
             final ProgressDialog dialog = new ProgressDialog(this);
             dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);//设置水平进度条样式
             dialog.setMessage("正在加载中");
@@ -168,14 +179,61 @@ public class MainActivity extends AppCompatActivity{
             }, 0, 1000);//
             dialog.show();
         });
+
+        btn_7.setOnClickListener(v->{
+            //半自定义对话框
+            View view = getLayoutInflater().inflate(R.layout.half_dialog_view, null);
+            final EditText editText = (EditText) view.findViewById(R.id.edt_halfdialog);
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setIcon(R.mipmap.icon)//设置标题的图片
+                    .setTitle("半自定义对话框")//设置对话框的标题
+                    .setView(view)//绑定自定义定义布局
+                    .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String content = editText.getText().toString();
+                            Toast.makeText(MainActivity.this, content, Toast.LENGTH_SHORT).show();
+                            dialog.dismiss();
+                        }
+                    }).create();
+            dialog.show();
+        });
+
+        btn_8.setOnClickListener(v->{
+            //style自定义对话框
+            Dialog dialog = new Dialog(this, R.style.NormalDialogStyle);
+            View view = View.inflate(this, R.layout.dialog_normal, null);
+            dialog.setContentView(view);
+            dialog.setCanceledOnTouchOutside(true);
+
+//            view.setMinimumHeight((int) (ScreenSizeUtils.getInstance(this).getScreenHeight() * 0.23f));
+//            Window dialogWindow = dialog.getWindow();
+//            WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+//            lp.width = (int) (ScreenSizeUtils.getInstance(this).getScreenWidth() * 0.9f);
+//            lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+//            lp.gravity = Gravity.BOTTOM;
+//            dialogWindow.setAttributes(lp);
+
+            dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+            dialog.show();
+        });
     }
 
     private void initView() {
-        btn_1 = findViewById(R.id.btn_1);
-        btn_2 = findViewById(R.id.btn_2);
-        btn_3 = findViewById(R.id.btn_3);
-        btn_4 = findViewById(R.id.btn_4);
-        btn_5 = findViewById(R.id.btn_5);
-        btn_6 = findViewById(R.id.btn_6);
+        btn_1 = findViewById(R.id.btn_1);//普通对话框
+        btn_2 = findViewById(R.id.btn_2);//列表对话框
+        btn_3 = findViewById(R.id.btn_3);//单选对话框
+        btn_4 = findViewById(R.id.btn_4);//多选对话框
+        btn_5 = findViewById(R.id.btn_5);//圆形加载框
+        btn_6 = findViewById(R.id.btn_6);//条形加载框
+        btn_7 = findViewById(R.id.btn_7);//半自定义对话框
+        btn_8 = findViewById(R.id.btn_8);//全自定义对话框
     }
 }
