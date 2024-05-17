9825a25b603ab4e28a6d4d3a25c4572663bbf5fc
ikunhuaji <1550309783@qq.com>
2024-05-17 08:56:24 +0800

    new 1

--- avatar/src/main/java/com/software/avatar/activity/LoginActivity.java
+++ avatar/src/main/java/com/software/avatar/activity/LoginActivity.java
@@ -2,15 +2,50 @@ package com.software.avatar.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.software.avatar.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText edt_phone;
+    private EditText edt_password;
+    private Button btn_login;
+    private TextView tv_register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initViews();
+
+        initEvents();
+    }
+
+    private void initEvents() {
+        tv_register.setOnClickListener(v->{
+            Intent intent = new Intent(
+                    LoginActivity.this,
+                    RegisterActivity.class
+            );
+
+            startActivity(intent);
+        });
+
+        btn_login.setOnClickListener(v->{
+
+        });
+    }
+
+    private void initViews() {
+        edt_phone = findViewById(R.id.edt_phone);
+        edt_password = findViewById(R.id.edt_password);
+        btn_login = findViewById(R.id.btn_login);
+        tv_register = findViewById(R.id.tv_register);
     }
 }
